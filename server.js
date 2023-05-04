dc6785b30ee435b37eb419c2d987f807e570369f
Airel Donaire <aireldonaire@gmail.com>
2023-05-04 13:22:48 -0700

    added port and mongo connections

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const { Collection } = require('mongodb')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
 
 let db,
     dbConnectionString = process.env.DB_STRING
-    dbName = '',
-    collection
+    dbName = 'star-wars-quotes',
+    Collection
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log('Connected to Database')
+        db = client.db(dbName)
+        collection = db.collection('quotes')
+    })
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server is running`)
+}
+)
