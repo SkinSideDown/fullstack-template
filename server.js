8e3f7346f9c25124e1c17e394b4f22c0e547ccbd
Airel Donaire <aireldonaire@gmail.com>
2023-05-04 13:32:34 -0700

    added middleware

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const { Collection } = require('mongodb')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
 
+//MongoDB
 let db,
     dbConnectionString = process.env.DB_STRING
     dbName = 'star-wars-quotes',
@@ -17,6 +18,15 @@ MongoClient.connect(dbConnectionString)
         collection = db.collection('quotes')
     })
 
+//Middleware
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
+
+
+//Port
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server is running`)
 }
